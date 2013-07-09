06de002bd45bee3ebcb00dc2ca9509fe7be5ebb0
Nicolas Danet <nicolasdanet@new-host-3.livebox.home>
2013-07-09 18:36:14 +0200

    twitter

--- reverse2.rb
+++ reverse2.rb
@@ -1,5 +1,11 @@
 
-#A FAIRE : mise en forme css + serveur web + tweet à la fin
+#A FAIRE : mise en forme css + tweet à la fin
+
+# A chaque mise à jour (dans la racine de l'appli): 
+  # 1. git add .
+  # 2. git commit -m "maj n°x"
+  # 3. git push heroku master
+  # 3.bis git push origin master
 
 #NTenadification
 
