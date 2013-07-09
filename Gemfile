4c4606c077b99ac93821d7f1eca52eaa64fac1c7
Nicolas Danet <nicolasdanet@new-host-3.livebox.home>
2013-07-09 19:18:35 +0200

    lissage4

--- Gemfile
+++ Gemfile
@@ -1,5 +1,4 @@
 source 'https://rubygems.org'
-gem 'sinatra'
-gem 'pry'
-gem 'shotgun'
-gem 'twitter'
+require "pry"
+require "shotgun"
+require "sinatra"
--- reverse2.rb
+++ reverse2.rb
@@ -1,11 +1,5 @@
 
-#A FAIRE : mise en forme css + tweet à la fin
-
-# A chaque mise à jour (dans la racine de l'appli): 
-  # 1. git add .
-  # 2. git commit -m "maj n°x"
-  # 3. git push heroku master
-  # 3.bis git push origin master
+#A FAIRE : mise en forme css
 
 #NTenadification
 
